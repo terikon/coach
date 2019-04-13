8bfd9f665614fcd81a893c99302398fff8260d71
viskin <r.viskin@terikon.com>
2019-04-13 10:07:15 +0300

    webrtc and player events

--- app/video.js
+++ app/video.js
@@ -19,20 +19,69 @@ window.addEventListener('load', () => {
         videoElement.src = URL.createObjectURL(file);
     });
 
+    videoElement.addEventListener('play', onPlay);
+    videoElement.addEventListener('pause', onPause);
+    videoElement.addEventListener('seeked', onSeeked);
+
     const servers = null; // Allows for RTC server configuration.
+    const pcConstraint = null;
+    const dataConstraint = null;
+    let receiveChannel;
 
     // Create peer connections and add behavior.
-    localPeerConnection = new RTCPeerConnection(servers);
-    trace('Created local peer connection object localPeerConnection.');
+    let localPeerConnection = new RTCPeerConnection(servers, pcConstraint);
     localPeerConnection.addEventListener('icecandidate', handleConnection);
     localPeerConnection.addEventListener('iceconnectionstatechange', handleConnectionChange);
+    trace('Created local peer connection object localPeerConnection.');
+    let sendChannel = localPeerConnection.createDataChannel('sendDataChannel', dataConstraint);
+    sendChannel.addEventListener('open', onSendChannelStateChange);
+    sendChannel.addEventListener('close', onSendChannelStateChange);
+    trace('Created send data channel');
 
     //localPeerConnection.addStream();
 
-    remotePeerConnection = new RTCPeerConnection(servers);
+    let remotePeerConnection = new RTCPeerConnection(servers, pcConstraint);
     trace('Created remote peer connection object remotePeerConnection.');
     remotePeerConnection.addEventListener('icecandidate', handleConnection);
     remotePeerConnection.addEventListener('iceconnectionstatechange', handleConnectionChange);
+    remotePeerConnection.addEventListener('datachannel', receiveChannelCallback);
+
+    localPeerConnection.createOffer().then(
+        gotDescription1,
+        onCreateSessionDescriptionError
+    );
+
+    function onPlay() {
+        console.log('play');
+        sendData('play');
+    }
+
+    function onPause() {
+        console.log('pause');
+        sendData('pause');
+    }
+
+    function onSeeked() {
+        console.log('seeked');
+    }
+
+    function sendData(data) {
+        sendChannel.send(data);
+        trace('Sent Data: ' + data);
+    }
+
+    function closeDataChannels() {
+        trace('Closing data channels');
+        sendChannel.close();
+        trace('Closed data channel with label: ' + sendChannel.label);
+        receiveChannel.close();
+        trace('Closed data channel with label: ' + receiveChannel.label);
+        localPeerConnection.close();
+        remotePeerConnection.close();
+        localPeerConnection = null;
+        remotePeerConnection = null;
+        trace('Closed peer connections');
+    }
 
     function handleConnection(event) {
         const peerConnection = event.target;
@@ -56,8 +105,8 @@ window.addEventListener('load', () => {
     function handleConnectionSuccess(peerConnection) {
         trace(`${getPeerName(peerConnection)} addIceCandidate success.`);
     };
-      
-      // Logs that the connection failed.
+
+    // Logs that the connection failed.
     function handleConnectionFailure(peerConnection, error) {
         trace(`${getPeerName(peerConnection)} failed to add ICE Candidate:\n${error.toString()}.`);
     }
@@ -66,6 +115,59 @@ window.addEventListener('load', () => {
         const peerConnection = event.target;
         console.log('ICE state change event: ', event);
         trace(`${getPeerName(peerConnection)} ICE state: ${peerConnection.iceConnectionState}.`);
+    }
+
+    function onSendChannelStateChange() {
+        var readyState = sendChannel.readyState;
+        trace(`Send channel state is: ${readyState}`);
+        if (readyState === 'open') {
+            //dataChannelSend.disabled = false;
+            //dataChannelSend.focus();
+            //sendButton.disabled = false;
+            //closeButton.disabled = false;
+        } else {
+            //dataChannelSend.disabled = true;
+            //sendButton.disabled = true;
+            //closeButton.disabled = true;
+        }
+    }
+
+    function gotDescription1(desc) {
+        localPeerConnection.setLocalDescription(desc);
+        trace(`Offer from localPeerConnection \n${desc.sdp}`);
+        remotePeerConnection.setRemoteDescription(desc);
+        remotePeerConnection.createAnswer().then(
+            gotDescription2,
+            onCreateSessionDescriptionError
+        );
+    }
+
+    function gotDescription2(desc) {
+        remotePeerConnection.setLocalDescription(desc);
+        trace(`Answer from remotePeerConnection \n${desc.sdp}`);
+        localPeerConnection.setRemoteDescription(desc);
+    }
+
+    function onCreateSessionDescriptionError(error) {
+        trace(`Failed to create session description: ${error.toString()}`);
+    }
+
+    function receiveChannelCallback(event) {
+        trace('Receive Channel Callback');
+        receiveChannel = event.channel;
+        receiveChannel.addEventListener('message', onReceiveMessageCallback);
+        receiveChannel.addEventListener('open', onReceiveChannelStateChange);
+        receiveChannel.addEventListener('close', onReceiveChannelStateChange);
+    }
+
+    function onReceiveMessageCallback(event) {
+        trace(`Received Message: ${event.data}`);
+        //dataChannelReceive.value = event.data;
+    }
+
+    function onReceiveChannelStateChange() {
+        var readyState = receiveChannel.readyState;
+        trace(`Receive channel state is: ${readyState}`);
     }
 
     function getPeerName(peerConnection) {
