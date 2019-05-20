e352f0ac33a3f551fac2c67b212e4b43e13a1874
viskin <r.viskin@terikon.com>
2019-05-20 10:02:13 +0300

    initial layout-extension

---
+++ layout-extension/popup.js
@@ -0,0 +1,37 @@
+let changeColor = document.getElementById('changeColor');
+let selectTitle = document.getElementById('selectTitle');
+
+chrome.storage.sync.get('color', function (data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+  changeColor.onclick = function (element) {
+    let color = element.target.value;
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function (tabs) {
+      chrome.tabs.executeScript(
+        tabs[0].id, {
+          //code: 'document.body.style.backgroundColor = "' + color + '";'
+          file: 'contentScript-toggleOthers.js'
+        });
+    });
+  };
+});
+
+selectTitle.addEventListener('change', function () {
+  let selected = this.value;
+  let title = 'Hangouts Video Call';
+  if (selected > 0) {
+    title = `Student - ${selected}`;
+  }
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function (tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id, {
+        code: `document.title = '${title}';`
+      });
+  });
+});
