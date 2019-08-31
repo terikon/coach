46d0016cc5927f7b6f0cc87380cbb3929477fb9c
viskin <r.viskin@terikon.com>
2019-08-31 21:18:19 +0300

    better components

--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
+Vue.use(window.httpVueLoader);
+
 import './aaa.js';
-//import './aaa.vue';
 
 const app = new Vue({
   el: '#app',
   components: {
-    'aaa-component': window.httpVueLoader('aaa.vue'),
+    'aaa-component': 'url:aaa.vue',
   }
 });
