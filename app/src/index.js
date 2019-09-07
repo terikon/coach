33fa403ecaf9617908a4b07802b2a771bbfa417c
viskin <r.viskin@terikon.com>
2019-09-07 20:02:09 +0300

    index compiles

--- app/src/index.js
+++ app/src/index.js
@@ -5,12 +5,3 @@ import App from './App.vue'
 new Vue({
   render: function (h) { return h(App) },
 }).$mount('#app')
-
-import './aaa.js';
-
-const app = new Vue({
-  el: '#app',
-  components: {
-    'aaa-component': 'url:aaa.vue',
-  }
-});
